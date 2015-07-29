d0f1cdf88786c88e7e61dd006b26634365370894
Thiago Andrade <thiagoh@gmail.com>
2015-07-29 13:15:55 -0300

    shared pointers

---
+++ src/core/SharedPtr.cpp
@@ -0,0 +1,7 @@
+/*
+ * SharedPtr.cpp
+ *
+ *  Created on: Jul 27, 2015
+ *      Author: thiago
+ */
+
---
+++ src/core/SharedPtr.h
@@ -0,0 +1,93 @@
+/*
+ * SharedPtr.h
+ *
+ *  Created on: Jul 27, 2015
+ *      Author: thiago
+ */
+
+#ifndef SRC_CORE_SHAREDPTR_H_
+#define SRC_CORE_SHAREDPTR_H_
+
+#include <stdio.h>
+
+/*
+ * Implementing a simple smart pointer in C++
+ * http://www.codeproject.com/Articles/15351/Implementing-a-simple-smart-pointer-in-c
+ */
+
+template<typename T>
+class SharedPtr {
+public:
+	SharedPtr(T* ptr):
+		_ptr(ptr), _c(0) {
+		_c = new Counter(0);
+		_c->addRef();
+	}
+
+	SharedPtr(const SharedPtr& rhs) :
+		_ptr(rhs._ptr), _c(rhs._c) {
+		_addRef(rhs);
+	}
+
+	SharedPtr& operator=(const SharedPtr& rhs) {
+		if (this == &rhs) {
+			printf("I've fallen into identity test!\n");
+			return *this;
+		}
+		_c->releaseRef();
+		_addRef(rhs);
+		return *this;
+	}
+
+	virtual ~SharedPtr() {
+		_c->releaseRef();
+		if (_c->get() == 0) {
+			delete _ptr;
+			delete _c;
+			printf("DO deleting ptr\n");
+		} else {
+			printf("NOT deleting ptr\n");
+		}
+	}
+
+	T* get() {
+		return _ptr;
+	}
+private:
+
+	void _addRef(const SharedPtr& rhs) {
+		_ptr = rhs._ptr;
+		_c = rhs._c;
+		_c->addRef();
+	}
+
+	class Counter {
+	public:
+		Counter(unsigned int c): _refs(c) {
+			printf("Counter ctor\n");
+		};
+		Counter(const Counter& rhs): _refs(rhs._refs) {
+			printf("Counter copy ctor\n");
+		};
+
+		~Counter() {
+		}
+		unsigned int get() {
+			return _refs;
+		}
+		void addRef() {
+			++_refs;
+		}
+
+		void releaseRef() {
+			--_refs;
+		}
+
+	private:
+		unsigned int _refs;
+	};
+	T* _ptr;
+	Counter* _c;
+};
+
+#endif /* SRC_CORE_SHAREDPTR_H_ */
